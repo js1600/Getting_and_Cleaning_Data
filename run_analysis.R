a24550f6de8df6c83c44e2b761c1ef67bd28d4a4
js1600 <js1600@users.noreply.github.com>
2015-01-21 21:49:20 -0600

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -16,43 +16,36 @@ subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
 
 subject_combined <- rbind(subject_test, subject_train)
 
+X_comb <- rbind(X_test, X_train)
+y_comb <- rbind(y_test, y_train)
+
 # Load links to the class labels with their activity name
 # Read the table and give me the second column.
-activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")[,2]
+activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")
 # Load the list of all features
-features <- read.table("./UCI HAR Dataset/features.txt")[,2]
+features <- read.table("./UCI HAR Dataset/features.txt")
 # 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
 # Get all Features whose names have "Mean or std" in them.
-extract_measurements <- grepl("mean|std", features)
+extract_measurements <- grep("-mean\\(\\)|-std\\(\\)", features[, 2])
 # Add the features names to the X_test and X_train data.
-names(X_test) = features
-names(X_train) = features
 # Now only take those items that have the Features grepped for previously.
-X_test = X_test[,extract_measurements]
-X_train = X_train[,extract_measurements]
+X_comb <- X_comb[ ,extract_measurements]
+names(X_comb) <- features[extract_measurements, 2]
+names(X_comb) <- gsub("\\(|\\)", "", names(X_comb))
+names(X) <- tolower(names(X))  
 
 # 3. Uses descriptive activity names to name the activities in the data set
-y_test[,2] = activity_labels[y_test[,1]]
-y_train[,2] = activity_labels[y_train[,1]]
-
+y_comb[,1] = activity_labels[y_comb[,1], 2]
+names(y_comb) <- "activity"
 
 # 4. Appropriately label the data set with descriptive variable names. 
-names(y_test) = c("Activity_ID", "Activity_Label")
-names(subject_test) = "subject"
-names(y_train) = c("Activity_ID", "Activity_Label")
-names(subject_train) = "subject"
+names(subject_combined) <- "subject"
 
-
-test_data_table <- cbind(subject_test, y_test, X_test)
-train_data_table <- cbind(subject_train, y_train, X_train)
 #Bind the data tables
-clean_data = rbind(test_data_table, train_data_table)
-
+clean_data = cbind(subject_combined,y_comb,X_comb)
 write.table(clean_data, file = "./merged_clean_data.txt") 
 
 # 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-
-activity_labels <- as.data.frame(activity_labels)
 uniqueSubjects = unique(subject_combined)[,1]
 numSubjects = length(unique(subject_combined)[,1])
 numActivities = length(activity_labels[,1])
@@ -65,7 +58,7 @@ for (s in 1:numSubjects) {
 		result[row, 1] = uniqueSubjects[s]
 		result[row, 2] = activity_labels[a, 1]
 		tmp <- clean_data[clean_data$subject==s , ]
-		#tmp <- clean_data[clean_data$subject==s & clean_data$Activity_Label==activity_labels[a, 2], ]
+		tmp <- clean_data[clean_data$subject==s & clean_data$activity==activity_labels[a, 2], ]
 		result[row, 3:numCols] <- colMeans(tmp[, 3:numCols])
 		row = row+1
 	}
